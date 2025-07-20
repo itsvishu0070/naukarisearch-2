9797aa56e2e41583fddc54a06bbfe63f6a209940
itsvishu0070 <panditvishu003@gmail.com>
2025-07-20 23:40:11 +0530

    Update user controller and API endpoints; modify .gitignore and package.json

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: "https://naukri-search.vercel.app",
+  credentials: true,
+  
+
+};
 
 app.use(cors(corsOptions));
 
